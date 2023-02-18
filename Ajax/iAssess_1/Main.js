de47eba3fcbc88950250ba95d3d6fefe637358f7
Prajwal Yelne <78864206+PrajwalYelne@users.noreply.github.com>
2023-02-18 11:09:19 +0530

    iAssess_1

--- Ajax/iAssess_1/Main.js
+++ Ajax/iAssess_1/Main.js
@@ -1,17 +1,20 @@
 <!DOCTYPE html>
 <html lang="en">
+    
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 </head>
+
 <body>
     <h1>Ajax</h1>
         <button type="button" id="getPlaylist" onclick="loadDoc()">Get Playlist</button>>
         <div id="playlist">
             <table id="demo"></table>
         </div>
+
     <script>
         function loadDoc() {
               var xhttp = new XMLHttpRequest();
