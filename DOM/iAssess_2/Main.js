985be9a9d9620ed18c44bb6c2ab364f0fd6a258b
Prajwal Yelne <78864206+PrajwalYelne@users.noreply.github.com>
2023-03-31 11:24:23 +0530

    iAssess_2

---
+++ DOM/iAssess_2/Main.js
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html>
+<style>
+    body {
+        text-align: center;
+    }
+    
+    table,
+    th,
+    td {
+        width: 50%;
+        font-size: 18px;
+        border: 1px solid black;
+        border-collapse: collapse;
+    }
+    
+    tbody tr:nth-child(odd) {
+        background-color: #e0dfdf;
+    }
+    
+    table.center {
+        margin-left: auto;
+        margin-right: auto;
+    }
+    
+    button {
+        padding: 5px;
+        margin: 15px;
+        cursor: pointer;
+    }
+</style>
