74b9412136468718ffde67183cfa2c6978b0a929
Prajwal Yelne <78864206+PrajwalYelne@users.noreply.github.com>
2023-04-09 12:10:35 +0530

    iDesign_1

---
+++ Events/iDesign_1/Main.js
@@ -0,0 +1,65 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <style>
+        #div1 {
+            border: 1px solid black;
+            width: 200px;
+            height: 100px;
+            background-color: none;
+            text-align: center;
+        }
+        
+        #div2 {
+            border: 1px solid black;
+            width: 200px;
+            height: 100px;
+            background-color: none;
+            text-align: center;
+        }
+        
+        #div3 {
+            border: 1px solid black;
+            width: 200px;
+            height: 100px;
+            background-color: none;
+            text-align: left;
+        }
+        
+        #div4 {
+            border: 1px solid black;
+            width: 200px;
+            height: 100px;
+            background-color: none;
+            text-align: left;
+        }
+        
+        .class1 {
+            font-size: 30px;
+            background-color: #20c5b5;
+            color: white;
+            text-align: center;
+        }
+        
+        .class2 {
+            font-size: 30px;
+            background-color: #fa8072;
+            color: white;
+            text-align: center;
+        }
+        
+        .class3 {
+            font-size: 30px;
+            background-color: #7d8c9b;
+            color: white;
+            text-align: center;
+        }
+        
+        .class4 {
+            font-size: 30px;
+            background-color: #7ba428;
+            color: white;
+            text-align: center;
+        }
+    </style>
+</head>
