d0ef48b5d7e536bb4208a426d902503c3c124240
Prajwal Yelne <78864206+PrajwalYelne@users.noreply.github.com>
2023-04-29 21:27:05 +0530

    iAssess_2

--- Events/iAssess_2/Main.js
+++ Events/iAssess_2/Main.js
@@ -5,3 +5,8 @@
             alert('Cut/Copy/Paste is restricted.');
         }
     </script>
+<body>
+    <h2>Programming Contest</h2>
+    <textarea id="editor" name="" rows="10" cols="30" oncut="warning()" oncopy="warning()" onpaste="warning()"></textarea>
+</body>
+</html>
