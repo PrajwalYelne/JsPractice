f89e50f6461049878e4c40a6448e6f7d7959c601
Prajwal Yelne <78864206+PrajwalYelne@users.noreply.github.com>
2023-05-15 20:38:42 +0530

    iDesign_2

--- Js/iDesign_2/Main.js
+++ Js/iDesign_2/Main.js
@@ -1,33 +1,15 @@
-package ExceptionHandling.iDesign_2;
-
-import java.util.Scanner;
-
-public class Main {
-    public static void main(String[] args) {
-        Scanner in = new Scanner(System.in);
-
-        System.out.println("Enter the number of seats to be booked:");
-        int n = in.nextInt();
-        int s = 0;
-
-        try {
-            if (n > 100 || n < 0)
-                throw new ArrayIndexOutOfBoundsException();
-            int[] array = new int[n];
-            for (int i = 0; i < n; i++) {
-                System.out.println("Enter the seat number " + (i + 1));
-                s = in.nextInt();
-                if (s > 100 || s < 0)
-                    throw new ArrayIndexOutOfBoundsException();
-                else
-                    array[i] = s;
-            }
-
-            System.out.println("The seats booked are:");
-            for (int e : array)
-                System.out.println(e);
-        } catch (ArrayIndexOutOfBoundsException e) {
-            System.out.println("java.lang.ArrayIndexOutOfBoundsException: " + (s - 1));
-        }
-    }
+var fs = require('fs');
+var input = fs.readFileSync('input.txt').toString();
+//fill your code
+var op=Number(input);
+function checkAge(op){
+    if(op>=18)
+    return true;
+    else
+    return false;
 }
+let x=checkAge(op);
+if(x==true)
+console.log('Allowed');
+else
+console.log('Not Allowed');
