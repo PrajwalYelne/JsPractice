bfbe17cadb9f69510e2580165ab61b66fa8cc3c2
Prajwal Yelne <78864206+PrajwalYelne@users.noreply.github.com>
2023-06-02 11:35:30 +0530

    iAssess_1

--- Js/iAssess_1/Main.js
+++ Js/iAssess_1/Main.js
@@ -1,43 +1,19 @@
-// package CollectionsAndMap.iAssess_1;
-
-import java.util.*;
-
-public class Main {
-    public static void main(String[] args) {
-        Scanner in = new Scanner(System.in);
-        System.out.println("Enter number of users:");
-        int n = in.nextInt();
-        in.nextLine();
-
-        Set<User> set = new TreeSet<>();
-        for (int i = 0; i < n; i++) {
-            System.out.println("Enter details of user" + (i + 1));
-
-            System.out.println("Username:");
-            String username = in.nextLine();
-            System.out.println("Bank name:");
-            String bankname = in.nextLine();
-
-            set.add(new User(username, bankname));
-        }
-
-        System.out.println("Enter username(Expire in one month) seperated by comma");
-        String[] input = in.nextLine().split(",");
-
-        System.out.println("Users going to expire within a month");
-        ArrayList<User> toRetain = new ArrayList<>();
-
-        for (int i = 0; i < input.length; i++) {
-            toRetain.add(new User(input[i], null));
-        }
-
-        set.retainAll(toRetain);
-
-        int i = 1;
-        for (User u : set) {
-            System.out.println("User " + (i++));
-            System.out.println("User Name = " + u.getUsername());
-            System.out.println("Bank Name = " + u.getBankname());
-        }
-    }
+var fs = require('fs');
+var input = fs.readFileSync('input.txt').toString();
+//fill your code
+var op=input;
+function palindrome(str) {
+ var re = /[\W_]/g;
+ var lowRegStr=str.toLowerCase().replace(re,'');
+ var reverseStr = lowRegStr.split('').reverse().join('');
+ if( reverseStr === lowRegStr){
+     return true;
+ }
+ else 
+ return false;
 }
+let x=palindrome(op);
+if(x==true)
+console.log(op+" "+"is a palindrome");
+else
+console.log(op+" "+"is not a palindrome");
