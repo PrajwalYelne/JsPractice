b126885e4b885cc5d7b0f18981b9d50569ed4797
Prajwal Yelne <78864206+PrajwalYelne@users.noreply.github.com>
2023-08-20 11:44:18 +0530

    iAssess_2

--- Js/iAssess_2/Main.js
+++ Js/iAssess_2/Main.js
@@ -1,41 +1,15 @@
-// package CollectionsAndMap.iAssess_2;
+var fs = require('fs');
+var input = fs.readFileSync('input.txt').toString();
+//fill your code
 
-import java.util.Scanner;
+var num = Number(input);
 
-public class Main {
-    public static void main(String[] args) {
-        Scanner in = new Scanner(System.in);
-        System.out.println("Enter the number of User details to be added");
-        int n = in.nextInt();
-        in.nextLine();
+function isPrime(num)
+{
+    let count = 0;
 
-        UserBO list = new UserBO();
-        list.addAll(UserBO.getList());
-
-        for (int i = 0; i < n; i++) {
-            System.out.println("Enter the user " + (i + 1) + " detail in csv format");
-            String[] input = in.nextLine().split(",");
-            User u = new User(input[0], input[1], input[2], input[3]);
-            list.add(u);
-        }
-
-        System.out.println("Name                Contact Number      Username            Email               ");
-
-        for (User u : list) {
-            u.display();
-        }
-
-        System.out.println("Enter the range to be removed from array list");
-        int n1 = in.nextInt();
-        int n2 = in.nextInt();
-
-        list.removeUser(n1, n2);
-
-        System.out.println("Name                Contact Number      Username            Email               ");
-        for (User u : list) {
-            u.display();
-        }
-
-
+    for(let i = 1; i <= num; i++)
+    {
+        if (num % i == 0)
+            count++;
     }
-}
