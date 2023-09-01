66e77b5313125f3f9976fbce8e1e901df6565e03
Prajwal Yelne <78864206+PrajwalYelne@users.noreply.github.com>
2023-09-01 19:57:21 +0530

    iDesign_1

--- OOPS/iDesign_1/Main.js
+++ OOPS/iDesign_1/Main.js
@@ -13,3 +13,16 @@ function SuperHero(name, alias, planet){
     this.name = name;
     this.alias = alias;
     this.planet = planet;
+}
+
+SuperHero.prototype.WhoAmI = function(){
+    console.log(this.name + " " + this.alias + " is from the planet " + this.planet);
+}
+
+//console.log(input[0], input[1], input[2]);
+
+var Superman = new SuperHero(input1[0], input1[1], input1[2]);
+var BatMan = new SuperHero(input2[0], input2[1], input2[2]);
+
+Superman.WhoAmI();
+BatMan.WhoAmI();
