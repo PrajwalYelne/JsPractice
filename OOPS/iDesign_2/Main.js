be90311f6cedc15861161c7887e05803fcfaa9d1
Prajwal Yelne <78864206+PrajwalYelne@users.noreply.github.com>
2023-09-07 20:11:42 +0530

    iDesign_2

--- OOPS/iDesign_2/Main.js
+++ OOPS/iDesign_2/Main.js
@@ -1,33 +1,14 @@
-package ExceptionHandling.iDesign_1;
+var fs = require('fs');
+var input=fs.readFileSync('input.txt').toString().trim().split('\n');
+//fill your code
 
-import java.util.Scanner;
-
-public class Main {
-    public static void main(String[] args) {
-        Scanner in = new Scanner(System.in);
-
-        System.out.println("Enter the Item type details:");
-        System.out.println("Enter the name:");
-        String name = in.nextLine();
-
-        System.out.println("Enter the deposit:");
-        String depositStr = in.nextLine();
-        double deposit = 0;
-        try {
-            deposit = Double.parseDouble(depositStr);
-
-            System.out.println("Enter the cost per day:");
-            String costPerDayStr = in.nextLine();
-            double costPerDay = 0;
-            try {
-                costPerDay = Double.parseDouble(costPerDayStr);
-                ItemType i = new ItemType(name, deposit, costPerDay);
-                System.out.println(i.toString());
-            } catch (NumberFormatException e) {
-                System.out.println("java.lang.NumberFormatException: For input string: \"" + costPerDayStr + "\"");
-            }
-        } catch (NumberFormatException e) {
-            System.out.println("java.lang.NumberFormatException: For input string: \"" + depositStr + "\"");
-        }
+class User {
+    constructor(name){
+        this.name = name;
     }
+
+    getName(){
+        return this.name;
+    }
+
 }
