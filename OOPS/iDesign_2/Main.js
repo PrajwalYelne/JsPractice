f9f7678f78f2de0082e5277944a7914d4d083da2
Prajwal Yelne <78864206+PrajwalYelne@users.noreply.github.com>
2023-09-15 12:15:31 +0530

    iDesign_2

--- OOPS/iDesign_2/Main.js
+++ OOPS/iDesign_2/Main.js
@@ -12,3 +12,16 @@ class User {
     }
 
 }
+
+function checkNameLength(person){
+
+        let name = person.getName();
+
+        if (name.length > 4)
+            console.log(name);
+        else
+            console.log("Name is too short");
+    }
+
+var person = new User(input[0]);
+checkNameLength(person);
