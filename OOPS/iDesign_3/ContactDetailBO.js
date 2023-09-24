139f67a45a244e0ced4725ca4e40d45246afae10
Prajwal Yelne <78864206+PrajwalYelne@users.noreply.github.com>
2023-09-24 09:56:19 +0530

    iDesign_3

--- OOPS/iDesign_3/ContactDetailBO.js
+++
@@ -1,8 +0,0 @@
-package ExceptionHandling.iDesign_3;
-
-public class ContactDetailBO {
-    static void validate(String mobile, String alternateMobile) throws DuplicateMobileNumberException {
-        if (mobile.equals(alternateMobile))
-            throw new DuplicateMobileNumberException();
-    }
-}
