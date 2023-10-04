4b786926b041a5e9aa19190055fc3e2d127617fc
Prajwal Yelne <78864206+PrajwalYelne@users.noreply.github.com>
2023-10-04 19:57:57 +0530

    iDesign_3

--- OOPS/iDesign_3/Main.js
+++ OOPS/iDesign_3/Main.js
@@ -1,20 +1,14 @@
-import java.util.*;
-import java.text.*;
-import java.io.*;
-public class Main
-{
-	public static void main(String[] args) throws Exception{
-// 		System.out.println("Hello World");
-        InputStreamReader r = new InputStreamReader(System.in);
-        BufferedReader br  = new BufferedReader(r);
-        System.out.println("Enter the date to be formatted:(MM-dd-yyyy)");
-        SimpleDateFormat sdf = new SimpleDateFormat("MM-dd-yyyy");
-        Date d= sdf.parse(br.readLine());
-        sdf = new SimpleDateFormat("EEE, MMM d, yy");
-        System.out.println("Date Format with EEE, MMM d, yy : "+sdf.format(d));
-        sdf = new SimpleDateFormat("dd.MM.yyyy");
-        System.out.println("Date Format with dd.MM.yyyy : "+sdf.format(d));
-        sdf = new SimpleDateFormat("dd/MM/yyyy");
-        System.out.println("Date Format with dd dd/MM/yyyy : "+sdf.format(d));
-	}
-}
+var fs = require('fs');
+var input=fs.readFileSync('input.txt').toString().trim().split('\n');
+
+var input1 = input[0].trim().split(',');
+var input2 = input[1].trim().split(',');
+var input3 = input[2].trim().split(',');
+//fill your code
+
+class Shape{
+    constructor(name, nos, color){
+        this.name = name;
+        this.nos = nos;
+        this.color = color;
+    }
