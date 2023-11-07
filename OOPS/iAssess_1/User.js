b015199dde1239205f58a9db677d97f98dbedb31
Prajwal Yelne <78864206+PrajwalYelne@users.noreply.github.com>
2023-11-07 21:11:39 +0530

    iAssess_1

--- OOPS/iAssess_1/User.js
+++ OOPS/iAssess_1/User.js
@@ -5,8 +5,3 @@ import java.util.Objects;
 public class User implements Comparable<User>{
     private String username;
     private String bankname;
-
-    public User(String username, String bankname) {
-        this.username = username;
-        this.bankname = bankname;
-    }
