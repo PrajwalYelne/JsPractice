a65f93bd7ebbe9e23391d8f67acdeff069c77a28
Prajwal Yelne <78864206+PrajwalYelne@users.noreply.github.com>
2023-11-30 21:14:14 +0530

    iAssess_1

--- OOPS/iAssess_1/Main.js
+++ OOPS/iAssess_1/Main.js
@@ -1,10 +1,9 @@
-// package CollectionsAndMap.iAssess_1;
+
 
 public class Main {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         System.out.println("Enter number of users:");
-
         }
     }
 }
