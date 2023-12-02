d2352d11588271240c420c68f119a5c47b49b7ff
Prajwal Yelne <78864206+PrajwalYelne@users.noreply.github.com>
2023-12-02 19:01:52 +0530

    iAssess_1

--- OOPS/iAssess_1/Main.js
+++ OOPS/iAssess_1/Main.js
@@ -1,9 +1,7 @@
 
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner in = new Scanner(System.in);
-        System.out.println("Enter number of users:");
+
         }
     }
 }
