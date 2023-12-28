5094155dd8e0e3918b54b31d18112765432b445e
Prajwal Yelne <78864206+PrajwalYelne@users.noreply.github.com>
2023-12-28 19:10:11 +0530

    iAssess_2

--- OOPS/iAssess_2/UserBO.js
+++ OOPS/iAssess_2/UserBO.js
@@ -5,9 +5,6 @@ import java.util.ArrayList;
 public class UserBO extends ArrayList<User> {
     public static UserBO getList() {
         UserBO u = new UserBO();
-        u.add(new User("mohan Raja", "9874563210", "mohan", "mohan@abc.in"));
-        u.add(new User("arjun Ravi", "4324237", "arjun", "arjun@abc.in"));
-        return u;
     }
   }
 }
