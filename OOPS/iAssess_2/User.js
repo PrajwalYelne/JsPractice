bf4416e6bcae61f6f3782a179fdbc4bcc5372b34
Prajwal Yelne <78864206+PrajwalYelne@users.noreply.github.com>
2024-01-12 15:07:01 +0530

    iAssess_2

--- OOPS/iAssess_2/User.js
+++ OOPS/iAssess_2/User.js
@@ -6,7 +6,4 @@ public class User implements Comparable<User> {
     private String userName;
     private String email;
 
-    public User(String name, String contactNumber, String userName, String email) {
-        this.name = name;
-        this.contactNumber = contactNumber;
 }
