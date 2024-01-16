92843947887e751257ffbc79de81e895ad3a7647
Prajwal Yelne <78864206+PrajwalYelne@users.noreply.github.com>
2024-01-16 13:08:44 +0530

    iAssess_2

--- OOPS/iAssess_2/User.js
+++ OOPS/iAssess_2/User.js
@@ -1,7 +1,5 @@
-// package CollectionsAndMap.iAssess_2;
 
 public class User implements Comparable<User> {
     private String name;
-    private String contactNumber;
     private String userName;
 }
