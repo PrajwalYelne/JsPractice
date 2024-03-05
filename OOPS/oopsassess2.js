79622805eca662dc552a23f4541e3b87ce396161
Prajwal Yelne <78864206+PrajwalYelne@users.noreply.github.com>
2024-03-05 10:30:23 +0530

    iAssess_2

--- OOPS/oopsassess2.js
+++ OOPS/oopsassess2.js
@@ -5,7 +5,5 @@ var input=fs.readFileSync('input.txt').toString().trim().split('\n');
 class Employee{
     constructor(name, dept, DOJ){
         this.name = name;
-        this.dept = dept;
-        this.DOJ = DOJ;
     }
 }
