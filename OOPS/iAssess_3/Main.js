20350b4d91adfcb7bb9b8d40d012d4e30b2af926
Prajwal Yelne <78864206+PrajwalYelne@users.noreply.github.com>
2024-05-13 14:27:43 +0530

    iAssess_3

--- OOPS/iAssess_3/Main.js
+++ OOPS/iAssess_3/Main.js
@@ -37,8 +37,6 @@ public class Main {
         for (String state : map.keySet()) {
             System.out.println("\nState:" + state);
             Map<String, Integer> m = map.get(state);
-            for (String city : m.keySet()) {
-            }
         }
     }
 }
