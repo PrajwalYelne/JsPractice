7ecd034ef08c6da9704699d559927ed7a8e25caa
Prajwal Yelne <78864206+PrajwalYelne@users.noreply.github.com>
2024-05-26 20:50:47 +0530

    iAssess_3

--- OOPS/iAssess_3/Main.js
+++ OOPS/iAssess_3/Main.js
@@ -16,7 +16,6 @@ public class Main {
 
         for (int i = 0; i < n; i++) {
             System.out.println("Enter the address:");
-            String[] input = in.nextLine().split(",");
         }
     }
 }
