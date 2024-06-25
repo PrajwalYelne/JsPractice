4d8f893e60e03f73f4f2803f9a16c7982304615b
Prajwal Yelne <78864206+PrajwalYelne@users.noreply.github.com>
2024-06-25 21:47:13 +0530

    iAssess_3

--- OOPS/iAssess_3/Main.js
+++ OOPS/iAssess_3/Main.js
@@ -7,6 +7,5 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         // Map<state, Map<city, count>>
-        Map<String, Map<String, Integer>> map = new TreeMap<>();
     }
 }
